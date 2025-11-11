18943c080bed704c3aabaa634253c56c8dd6ab82
ikopke23 <144629825+ik6335@users.noreply.github.com>
2025-11-11 13:26:56 -0500

    added boostrap stuff, idk why the css is not working

--- lib/KopkeWeb.pm
+++ lib/KopkeWeb.pm
@@ -3,12 +3,24 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index' => { 'title' => 'KopkeWeb' };
+get '/' => sub {    
+    template 'index' => { 
+        'title' => 'KopkeWeb',
+        'version' => "$VERSION"
+    };
 };
 
 get '/about' => sub {
-    template 'about' => { 'title' => 'KopkeWeb' };
+    template 'about' => { 
+        'title' => 'KopkeWeb',
+        'version' => "$VERSION"
+    };
+};
+
+get '/testing123/:test' => sub {
+    my $test = params->{test};
+    print "$test";
+    # print testing things for fun
 };
 
 true;
